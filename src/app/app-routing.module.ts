1f56b554b27c8d01713f806ef982d4685963c068
ryma1107 <jebrirym91@gmail.com>
2020-09-12 13:03:37 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'cpt1',
+    component: Comp1Component
+  },
+  {
+    path:'cpt2',
+    component: Comp2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
